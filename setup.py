64cd2b569abc89f4061d55b3beb96f6971ab358d
ShellCode33 <shellcode33@protonmail.ch>
2022-09-28 00:44:55 +0200

    Initial setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='HASS Desktop Sensor',
+    version='0.1',
+    description='Desktop activity sensor for Home Assistant',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="home assistant desktop sensor idle activity",
+    license="MIT",
+    author='ShellCode',
+    author_email='shellcode33@protonmail.ch',
+    url='https://github.com/ShellCode33/HASS-Desktop-Sensor',
+    packages=find_packages(),
+    python_requires='>=3.7',
+
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: POSIX :: Linux",
+    ],
+
+    install_requires=[
+        "requests",
+        "psutil"
+    ],
+
+    entry_points={
+        "console_scripts": [
+            "hass_desktop_sensor = desktop_sensor.manager:main",
+        ]
+    }
+)
