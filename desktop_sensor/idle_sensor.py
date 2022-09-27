ebe8a1497a5bcc347ecc6fd5e17c84a4f6d2544a
ShellCode33 <shellcode33@protonmail.ch>
2022-09-28 00:42:20 +0200

    Add fully working idle sensor based on xidlehook

--- desktop_sensor/idle_sensor.py
+++ desktop_sensor/idle_sensor.py
@@ -1,6 +1,14 @@
 # coding: utf-8
 
-from subprocess import run
+import subprocess
+import logging
+import time
+from threading import Thread, Lock
+from typing import Union, Literal, Optional
+
+_last_known_status_lock = Lock()
+_last_known_status = "active" # type: Union[Literal["active"], Literal["idle"]]
+sensor_exception = None # type: Optional[IdleSensorException]
 
 SUPPORTED_IDLE_HELPERS = {
     "xidlehook",
@@ -9,18 +17,60 @@ SUPPORTED_IDLE_HELPERS = {
 class IdleSensorException(Exception):
     "Thrown when something went wrong with the idle sensor"
 
-def run_xidlehook() -> None:
-    try:
-        run(["xidlehook",
-             "--not-when-fullscreen",
-             "--not-when-audio",
-             "--timer", "120", "echo idle", "echo active"])
-    except FileNotFoundError:
-        raise IdleSensorException(f"xidlehook not found, make sure it's available")
+def _run_xidlehook(interval: int) -> None:
+    global sensor_exception
 
-def main(idle_helper: str) -> None:
+    try:
+        process = subprocess.Popen([
+            "xidlehook",
+            #"--not-when-fullscreen",
+            "--not-when-audio",
+            "--timer", str(interval),
+            "echo idle",
+            "echo active"
+        ], stdout=subprocess.PIPE)
+        time.sleep(0.2) # Give it some time to start properly
+    except FileNotFoundError:
+        sensor_exception = IdleSensorException(f"xidlehook not found, make sure it's available")
+        return
+
+    if process.stdout is None:
+        sensor_exception = IdleSensorException("stdout is not accessible")
+        return
+
+    logging.info("xidlehook started")
+
+    while process.poll() is None:
+        line = process.stdout.readline()
+        status = line[:-1].decode()
+
+        if status in {"idle", "active"}:
+            with _last_known_status_lock:
+                global _last_known_status
+                _last_known_status = status
+
+            logging.debug(f"Status set to {status}")
+
+        else:
+            logging.error(f"Unexpected status value: {status}")
+
+        time.sleep(0.5)
+
+    sensor_exception = IdleSensorException("xidlehook stopped running")
+
+def get() -> Union[Literal["idle"], Literal["active"]]:
+
+    if sensor_exception:
+        raise sensor_exception
+
+    with _last_known_status_lock:
+        return _last_known_status
+
+def setup(idle_helper: str, interval: int) -> None:
 
     if idle_helper not in SUPPORTED_IDLE_HELPERS:
         raise IdleSensorException(f"{idle_helper} helper is not supported")
 
-    eval(f"run_{idle_helper}")()
+    idle_helper_func = eval(f"_run_{idle_helper}")
+    Thread(target=idle_helper_func, daemon=True, args=(interval,)).start()
+    time.sleep(0.4) # Give some time to the thread to start properly
