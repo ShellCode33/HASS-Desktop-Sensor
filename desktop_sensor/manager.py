65b943704afa74ec44556738b8057441f8211e24
ShellCode33 <shellcode33@protonmail.ch>
2022-09-29 01:53:21 +0200

    HASS url and device name are now handled through the CLI instead of the environment (+ catch HTTP errors)

--- desktop_sensor/manager.py
+++ desktop_sensor/manager.py
@@ -14,7 +14,6 @@ from urllib.parse import urlparse
 from desktop_sensor import idle_sensor, resources_sensor
 from desktop_sensor.idle_sensor import SUPPORTED_IDLE_HELPERS, IdleSensorException
 
-REQUIRED_ENV_VARIABLE = {"HASS_DEVICE_NAME", "HASS_URL", "HASS_ACCESS_TOKEN"}
 SENSOR_MODULES = {idle_sensor, resources_sensor}
 
 def normalize_name(name: str) -> str:
@@ -25,9 +24,7 @@ def normalize_name(name: str) -> str:
     normalized_name = "_".join(normalized_name.split())
     return normalized_name
 
-def hass_update(sensors: List[Union[Sensor, BinarySensor]]) -> None:
-    hass_url = os.environ["HASS_URL"].strip()
-    hass_device_name = os.environ["HASS_DEVICE_NAME"].strip()
+def hass_update(hass_url: str, hass_device_name: str, sensors: List[Union[Sensor, BinarySensor]]) -> None:
     normalized_device_name = normalize_name(hass_device_name)
 
     # Normalize URL because apparently Home Assistant returns 404 if url is like : http://hass:8123//api (double /)
@@ -61,80 +58,55 @@ def hass_update(sensors: List[Union[Sensor, BinarySensor]]) -> None:
         if isinstance(sensor, Sensor):
             attributes.update({"unit_of_measurement": sensor.unit})
 
-        resp = requests.post(
-            endpoint,
-            headers={"Authorization": f"Bearer {hass_access_token}"},
-            json={"state": state, "attributes": attributes}
-        )
+        try:
+            resp = requests.post(
+                endpoint,
+                headers={"Authorization": f"Bearer {hass_access_token}"},
+                json={"state": state, "attributes": attributes}
+            )
 
-        if resp.status_code not in {200, 201}:
-            logging.error(f"HASS API returned {resp.content} (code: {resp.status_code})")
+            if resp.status_code not in {200, 201}:
+                logging.error(f"HASS API returned {resp.content} (code: {resp.status_code})")
 
-def process_sensors() -> None:
-    for sensor_module in SENSOR_MODULES:
-        sensors = sensor_module.get() # type: List[Union[BinarySensor, Sensor]]
-
-        if not isinstance(sensors, list):
-            sensors = [sensors]
-
-        hass_update(sensors)
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            logging.error("HTTP request to HASS failed, will retry later")
+            return
 
 def parse_cli() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Welcome to the desktop sensor utility')
 
-    # Hidden parameters, for debug purposes, use environment variables instead
-    parser.add_argument('--hass-device-name', help=argparse.SUPPRESS)
-    parser.add_argument('--hass-url', help=argparse.SUPPRESS)
-    parser.add_argument('--hass-access-token', help=argparse.SUPPRESS)
+    parser.add_argument('--hass-device-name', help="friendly name of the computer you're running this on")
+    parser.add_argument('--hass-url', help="URL (including port) to your Home Assistant instance")
 
     parser.add_argument('--idle-helper', choices=SUPPORTED_IDLE_HELPERS, default="xidlehook", help="the external binary that should be used to detect activity")
-    parser.add_argument('--idle-delay', type=int, help="the delay (in seconds) to wait before the user must be considered idle", default=30)
+    parser.add_argument('--idle-delay', type=int, help="the delay (in seconds) to wait before the user must be considered idle", default=60 * 3)
     parser.add_argument('--report-interval', type=int, help="the interval (in seconds) at which reports will be made to Home Assistant", default=5)
 
     args = parser.parse_args()
-
-    if args.hass_device_name:
-        os.environ["HASS_DEVICE_NAME"] = args.hass_device_name
-        logging.warning("Passing --hass-device-name is for debug purposes only ! "
-                        "You should use HASS_DEVICE_NAME environment variable instead")
-
-    if args.hass_url:
-        os.environ["HASS_URL"] = args.hass_url
-        logging.warning("Passing --hass-url is for debug purposes only ! "
-                        "You should use HASS_URL environment variable instead")
-
-    if args.hass_access_token:
-        os.environ["HASS_ACCESS_TOKEN"] = args.hass_access_token
-        logging.warning("Passing --hass-access-token is for debug purposes only ! "
-                        "You should use HASS_ACCESS_TOKEN environment variable instead")
-
     return args
-
-def check_environment() -> bool:
-    is_valid = True
-
-    for var in REQUIRED_ENV_VARIABLE:
-        if var not in os.environ:
-            logging.error(f"Missing from environment: '{var}'")
-            is_valid = False
-
-    return is_valid
 
 def main() -> None:
     log_level = os.environ.get('LOGLEVEL', 'INFO').upper()
     logging.basicConfig(level=log_level, format="[%(levelname)s] %(asctime)s %(message)s")
 
     args = parse_cli()
-    is_env_valid = check_environment()
 
-    if not is_env_valid:
+    if "HASS_ACCESS_TOKEN" not in os.environ:
+        logging.error("HASS_ACCESS_TOKEN environment variable missing")
         sys.exit(1)
 
     idle_sensor.setup(args.idle_helper, args.idle_delay)
 
     try:
         while True:
-            process_sensors()
+            for sensor_module in SENSOR_MODULES:
+                sensors = sensor_module.get() # type: List[Union[BinarySensor, Sensor]]
+
+                if not isinstance(sensors, list):
+                    sensors = [sensors]
+
+                hass_update(args.hass_url, args.hass_device_name, sensors)
+
             time.sleep(args.report_interval)
     except IdleSensorException as ise:
         logging.error(f"idle sensor failed: {ise}")
